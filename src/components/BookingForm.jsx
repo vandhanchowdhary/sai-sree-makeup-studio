22450fe206a92b560ef8fcee149ca90d3849fbc9
vandhanchowdhary <vandhanchowdhary@gmail.com>
2026-06-06 20:53:28 +0530

    Add booking form functionality

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,4 +1,60 @@
+import { useState } from "react";
+
 const BookingForm = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phoneNumber: "",
+    bookingDate: "",
+    eventDetails: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formattedDate = new Date(formData.bookingDate).toLocaleString(
+      "en-IN",
+      {
+        dateStyle: "full",
+        timeStyle: "short",
+      },
+    );
+
+    const message = 
+    `Hello Renuka,
+    
+    Here's my details for appointment,
+
+    Name: ${formData.fullName}
+    Phone: ${formData.phoneNumber}
+
+    Preferred Date & Time: ${formattedDate}
+  
+    Event Details:
+    ${formData.eventDetails}
+    
+    Please contact me regarding your availability.
+    `;
+
+    const whatsappUrl = `https://wa.me/919951743400?text=${encodeURIComponent(
+      message,
+    )}`;
+
+    window.open(whatsappUrl, "_blank");
+    setFormData({
+      fullName: "",
+      phoneNumber: "",
+      bookingDate: "",
+      eventDetails: "",
+    });
+  };
+
   return (
     <section className="bg-white py-16">
       <div className="mx-auto max-w-3xl px-4">
@@ -6,19 +62,28 @@ const BookingForm = () => {
           Book Appointment
         </h2>
 
-        <form onSubmit={(e) => e.preventDefault()} className="mt-10">
+        <form onSubmit={handleSubmit} className="mt-10">
           <input
             type="text"
-            name="client-name"
+            id="full-name"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
             placeholder="Full Name"
             className="w-full rounded-xl border p-4 my-2"
+            required
           />
 
           <input
             type="tel"
+            pattern="[0-9]{10}"
+            id="phone-number"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
             placeholder="Phone Number"
-            name="client-phone-number"
             className="w-full rounded-xl border p-4 my-2"
+            required
           />
 
           <label
@@ -30,14 +95,22 @@ const BookingForm = () => {
           <input
             type="datetime-local"
             id="booking-date"
+            name="bookingDate"
+            value={formData.bookingDate}
+            onChange={handleChange}
             className="w-full rounded-xl border px-4 py-3 mb-2"
+            required
           />
 
           <textarea
+            id="event-details"
+            name="eventDetails"
+            value={formData.eventDetails}
+            onChange={handleChange}
             rows="5"
             placeholder="Tell us about your event..."
-            name="event-details"
             className="w-full rounded-xl border p-4 my-2"
+            required
           />
 
           <button
